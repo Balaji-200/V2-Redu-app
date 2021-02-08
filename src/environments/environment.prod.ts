df4859b881dc7d2580bf75e6d493b7d46b409e10
Balaji Durai Masanam <baladurai420@gmail.com>
2021-02-08 11:46:23 +0530

    deployed on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: '',
+  apiURL: 'https://v2-redu-server.herokuapp.com',
   genCertiUrl:'https://asia-south1-v2redu-certificate.cloudfunctions.net/V2-Redu_function'
 };
